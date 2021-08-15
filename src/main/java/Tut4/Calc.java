7c7ad48ece1d8dd0921c3ba8be3a40db436d8d2b
Braden <braden3458@gmail.com>
2021-08-15 21:28:07 +1200

    First and hopefully last commit

---
+++ src/main/java/Tut4/Calc.java
@@ -0,0 +1,12 @@
+package Tut4;
+
+public class Calc {
+
+    public static int add(int x, int y){
+        return x + y;
+
+    }
+    public static int subtract(int x, int y) {
+        return x + y;
+    }
+}
---
+++ src/test/java/Tut4/TestCalc.java
@@ -0,0 +1,11 @@
+package Tut4;
+
+import org.junit.Test;
+
+public class TestCalc{
+
+    @Test
+    public void testcalc(){
+        System.out.println(Calc.add(5,7));
+    }
+}
